09cbfb3df855a726af393ac79fd66e197b59e1d3
Glitch (hello-express) <none>
2020-04-06 22:16:57 +0000

    🏉👱 Checkpoint ./server.js:13042267/1125

--- server.js
+++ server.js
@@ -78,34 +78,14 @@ app.use(express.static('public'));
 
 //when a client connects the socket is established and I set up all the functions listening for events
 io.on('connection', function (socket) {
-
-
-    //this bit (middleware?) catches all incoming packets
-    //I use to make my own lil rate limiter without unleashing 344525 dependencies
-    //a rate limiter prevents malicious flooding from a hacked client
-    socket.use((packet, next) => {
-        if (gameState.players[socket.id] != null) {
-            var p = gameState.players[socket.id];
-            p.floodCount++;
-            if (p.floodCount > PACKETS_PER_SECONDS) {
-                console.log(socket.id + " is flooding! BAN BAN BAN");
-
-
-                if (p.IP != "") {
-                    //comment this if you don't want to ban the IP
-                    banned.push(p.IP);
-                    socket.emit("errorMessage", "Flooding attempt! You are banned");
-                    socket.disconnect();
+var IP ="";
+if (socket.handshake.headers != null)
+                if (socket.handshake.headers["x-forwarded-for"] != null) {
+                    IP = socket.handshake.headers["x-forwarded-for"].split(",")[0];
                 }
 
-            }
-        }
-        next();
-    });
-
-
     //this appears in the terminal
-    console.log('A user connected');
+    console.log('A user connected '+IP);
   });
 
 
