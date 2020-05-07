092be8f348aa0b2b12f35d4d7bf7aa3b7e17043c
Glitch (hello-express) <none>
2020-05-07 13:52:56 +0000

    🏟🌘 Checkpoint ./data.js:15274016/1

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ module.exports.SETTINGS = {
   //minimum time between talk messages enforced by both client and server
   ANTI_SPAM: 1000,
   //shows up at first non lurking login
-  INTRO_TEXT: "CClick around to move!!!!!"
+  INTRO_TEXT: "Click around to move!!!!!"
 };
 
 module.exports.ROOMS = {
